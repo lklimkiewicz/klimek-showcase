98b47c4300e79b132558b6680f248eab2249cf97
Lukasz Klimkiewicz <lklimkiewicz@Mac-mini-Lukasz.local>
2016-04-06 06:30:02 +0200

    first commit

---
+++ klimek-showcase/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  klimek-showcase
+//
+//  Created by Lukasz Klimkiewicz on 05.04.2016.
+//  Copyright © 2016 Lukasz Klimkiewicz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR : CGFloat = 157.0 / 255.0
+
---
+++ klimek-showcase/MaterialTextField.swift
@@ -0,0 +1,26 @@
+//
+//  MaterialTextField.swift
+//  klimek-showcase
+//
+//  Created by Lukasz Klimkiewicz on 06.04.2016.
+//  Copyright © 2016 Lukasz Klimkiewicz. All rights reserved.
+//
+
+import UIKit
+
+class MaterialTextField: UITextField {
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+        layer.borderColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.1).CGColor
+        layer.borderWidth = 1.0
+    }
+
+    //for placeholder 
+    override func textRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, 10, 0)
+    }
+    
+    override func editingRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, 10, 0)
+    }
+}
